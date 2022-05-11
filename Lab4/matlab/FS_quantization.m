6077398289f3b8824d67ac4322536181b74ba822
Dawid <dawikan506@student.polsl.pl>
2022-05-11 23:56:52 +0200

    Just before Lab4

--- Lab4/matlab/FS_quantization.m
+++ Lab4/matlab/FS_quantization.m
@@ -3,6 +3,8 @@
 
 function I1 = FS_quantization(I1, divider)
 
+    
+
     % quantization function select
     if class(divider) == "string" && divider == "bin"
         quantization = @(pix, divider) image_binarization(pix);
@@ -11,7 +13,8 @@ function I1 = FS_quantization(I1, divider)
     end
 
     tic
-    className = class(I1);
+    %className = class(I1);
+    [I1, className] = change_class(I1);
     
     % add border to image to prevent error with indexing
     I1 = [I1(:,1,:), I1, I1(:,end,:)];
@@ -37,11 +40,16 @@ function I1 = FS_quantization(I1, divider)
             timeMeasure(1) = timeMeasure(1) + toc;
             tic;
             I1(y,x,:) = new;
-            I1(y-1:y+1, x-1:x+1, :) = ...
-                cast( ...
-                    double(I1(y-1:y+1, x-1:x+1, :)) + mask .* err, ...
-                    className ...
-                );
+            % I1(y-1:y+1, x-1:x+1, :) = ...
+            %     cast( ...
+            %         double(I1(y-1:y+1, x-1:x+1, :)) + mask .* err, ...
+            %         className ...
+            %     );
+
+            tmp = I1(y-1:y+1, x-1:x+1, :) + mask .* err;
+            tmp = min(tmp, 1);
+            tmp = max(tmp, 0);
+            I1(y-1:y+1, x-1:x+1, :) = tmp;
 
             timeMeasure(2) = timeMeasure(2) + toc;
         end
@@ -50,11 +58,58 @@ function I1 = FS_quantization(I1, divider)
     timeMeasure
     I1 = I1(2:end-1, 2:end-1, :);
 
+    I1 = revert_change(I1, className);
+
 end
 
 
 
 
+
+function [I, classname] = change_class(I)
+
+    if isa(I,"double")
+        classname = "double";
+        return
+    end
+
+    if isa(I,"uint8")
+        classname = "uint8";
+        I = double(I)./ (2^8 - 1);
+        return
+    end
+
+    if isa(I,"uint16")
+        classname = "uint16";
+        I = double(I)./ (2^16 - 1);
+        return
+    end
+
+    if isa(I,"uint32")
+        classname = "uint32";
+        I = double(I)./ (2^32 - 1);
+        return
+    end
+end
+
+
+function I = revert_change(I, c)
+
+    if c == "double"
+        % do nothing
+        return
+    end
+    if c == "uint8"
+        I = uint8(I .* (2^8 - 1));
+    end
+    if c == "uint16"
+        I = uint16(I .* (2^16 - 1));
+    end
+    if c == "uint32"
+        I = uint32(I .* (2^32 - 1));
+    end
+
+end
 
 
 
